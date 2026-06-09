9464f38afde5b6a0272074597af37b8e6d03d0d5
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-09 16:00:57 +0300

    add serializers for users and login

--- user/serializers.py
+++ user/serializers.py
@@ -1,1 +1,48 @@
-# write your code here
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "email", "is_staff")
+        read_only_fields = ("id", "is_staff")
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=5,
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "password", "email")
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+        user = authenticate(
+            request=self.context.get("request"),
+            username=username,
+            password=password,
+        )
+
+        if not user:
+            raise serializers.ValidationError("Unable to authenticate with provided credentials")
+
+        attrs["user"] = user
+        return attrs
