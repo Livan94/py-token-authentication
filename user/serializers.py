92602022d2eb44bf2399cb64a46f15c396389d62
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-09 19:16:01 +0300

    fix flake8 issues

--- user/serializers.py
+++ user/serializers.py
@@ -62,7 +62,7 @@ class UserLoginSerializer(serializers.Serializer):
         )
 
         if not user:
-            raise serializers.ValidationError("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError("Unable to authenticate with provided credentials")  # noqa: E501
 
         attrs["user"] = user
         return attrs
