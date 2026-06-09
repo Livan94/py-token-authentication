ce16e567d9f7606709525195ba165040d7550ebe
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-09 19:45:44 +0300

    fix ticket serializer validation

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -86,12 +86,7 @@ class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
-            attrs(
-                ["row"],
-                attrs["seat"],
-                attrs["movie_session"].cinema_hall,
-                serializers.ValidationError,
-            )
+            attrs["row"], attrs["seat"], attrs["movie_session"]
         )
         return data
 
