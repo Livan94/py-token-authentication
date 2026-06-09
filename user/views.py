dc8f1fdb635ba7f7d4b16ba654c01e60083d6f8e
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-09 16:32:39 +0300

    implement register login and profile endpoints

--- user/views.py
+++ user/views.py
@@ -1,1 +1,39 @@
-# write your code here
+from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    UserRegistrationSerializer,
+    UserLoginSerializer,
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+
+class LoginUserView(ObtainAuthToken):
+    serializer_class = UserLoginSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={"request": request},
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({"token": token.key})
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
